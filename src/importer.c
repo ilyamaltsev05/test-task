5a7872c3d2715c1039d3e9cd6f490f31e5553cc9
Ilya Maltsev <malcevilya5@gmail.com>
2025-10-08 22:01:46 +0000

    feat: importer to get sms format matrices to GraphBLAS

---
+++ src/importer.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <GraphBLAS.h>
+
+GrB_Info import_matrix(char *path, GrB_Matrix *matrix) {
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "%s", "Can't open file ");
+        fprintf(stderr, "%s\n", path);
+        return 1;
+    }
+
+    GrB_Index rows;
+    GrB_Index cols;
+    char some;
+    fscanf(file, "%" PRIu64 " %" PRIu64 " %c" "\n", &rows, &cols, &some);
+
+    GrB_Matrix_new(matrix, GrB_BOOL, rows, cols);
+
+    GrB_Index i;
+    GrB_Index j;
+    GrB_Index entry;
+    while (fscanf(file, "%"PRIu64  " %"PRIu64 " %"PRIu64 "\n", &i, &j, &entry) == 3) {
+        GrB_Matrix_setElement(*matrix, true, i, j);
+    }
+
+    return 0;
+}
+
+int main() {
+    GrB_init(1);
+    GrB_Matrix some;
+    return import_matrix("./some_matrix.sms", &some);
+}
